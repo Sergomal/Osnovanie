d8e6b3698712f72da178b3cfc5424aa3dbe39152
Sergey Malyshev <mal_sn@mail.ru>
2024-11-12 13:39:24 +0300

    M8_Z3 Reminder Added check(), play_snd()

--- M8/Z3_GitHub_и_напоминания_со_звуком/Reminder.py
+++ M8/Z3_GitHub_и_напоминания_со_звуком/Reminder.py
@@ -6,24 +6,44 @@ import time
 import pygame
 
 
-def set():
+def set_time():
+    global t
     rem = sd.askstring("Время напоминания", "Введите время в 24 часовом формате: ЧЧ:ММ")
     if rem:
         try:
             hour = int(rem.split(":")[0])
             minute = int(rem.split(":")[1])
             now = datetime.datetime.now()
-            # print(now.strftime("%I:%M %p"))
-            print(now)
+            # print(now)
             dt = now.replace(hour=hour, minute=minute)
-            # print(dt.strftime("%I:%M %p"))
-            print(dt)
+            # print(dt)
+            # print(now.timestamp())
             t = dt.timestamp()
+            # print(t)
+        except Exception as e:
+            mb.showerror("Error", f"Error: {e}")
+        check()
 
-            print(t)
-        except ValueError as e:
-            print(str(e))
 
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            play_snd()
+            t = 0
+        # print(t - now)
+    window.after(10000, check)
+
+
+def play_snd():
+    print("play_snd")
+    pygame.mixer.init()
+    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.play()
+
+
+t = 0
 
 window = Tk()
 window.geometry("500x500")
@@ -32,7 +52,7 @@ window.title("Напоминание")
 label = Label(text="Установите напоминание")
 label.pack(pady=10)
 
-set_button = Button(text="Установить напоминание", command=set)
+set_button = Button(text="Установить напоминание", command=set_time)
 set_button.pack()
 
 
