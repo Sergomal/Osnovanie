983066f6c23a35b34ae85f5258fb4c88cdf540fc
Sergey Malyshev <mal_sn@mail.ru>
2024-11-13 22:08:25 +0300

    M8_Z3 Reminder Добавлено: 1. установка времени без секунд 2. кнопка выкл. музыки 3. отображение времени напоминания 4. отображение текста напоминания

--- M8/Z3_GitHub_и_напоминания_со_звуком/Reminder.py
+++ M8/Z3_GitHub_и_напоминания_со_звуком/Reminder.py
@@ -15,11 +15,15 @@ def set_time():
             minute = int(rem.split(":")[1])
             now = datetime.datetime.now()
             # print(now)
-            dt = now.replace(hour=hour, minute=minute)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             # print(dt)
             # print(now.timestamp())
             t = dt.timestamp()
             # print(t)
+            text = sd.askstring("Текст напоминания", "Введите текст напоминания")
+            label.config(
+                text=f"Напоминание на\n{hour:02}:{minute:02}\nс текстом {text}"
+            )
         except Exception as e:
             mb.showerror("Error", f"Error: {e}")
         check()
@@ -37,23 +41,37 @@ def check():
 
 
 def play_snd():
-    print("play_snd")
+    global music
+    music = True
     pygame.mixer.init()
     pygame.mixer.music.load("reminder.mp3")
     pygame.mixer.music.play()
 
 
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text="Установить новое напоминание")
+
+
 t = 0
+music = False
+
 
 window = Tk()
 window.geometry("500x500")
 window.title("Напоминание")
 
-label = Label(text="Установите напоминание")
+label = Label(text="Установите напоминание", font=("Arial", 14))
 label.pack(pady=10)
 
 set_button = Button(text="Установить напоминание", command=set_time)
-set_button.pack()
+set_button.pack(pady=10)
+
+stop_button = Button(text="Выключить музыку", command=stop_music)
+stop_button.pack(pady=10)
 
 
 window.mainloop()
